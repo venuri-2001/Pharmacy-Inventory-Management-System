d7794278929c166abab15feef1628b9a11c62ec9
venuri-2001 <158830993+venuri-2001@users.noreply.github.com>
2025-06-26 10:06:10 +0530

    All commit

---
+++ Pharmacy Management System/src/dao/ConnectionProvider.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author User_1
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pharmacy?useSSL=false","root","venu12");
+            return con;
+        }
+         catch(Exception e){
+                System.out.println(e);
+                return null;
+                 
+                 }
+        }
+    }
+
---
+++ Pharmacy Management System/src/dao/PharmacyUtils.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author User_1
+ */
+public class PharmacyUtils {
+    public static String billPath = "E:\\netbeansprojects\\Bill\\";
+    
+}
